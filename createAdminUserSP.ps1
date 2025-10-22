84a67dfa77ef9a40ed9d635f13fc776588eaa063
Gabriel Amaral <gamaral@ad.sensapure.com>
2025-10-22 16:31:48 -0600

    adding createAdminUserSP.ps1 to Repository

---
+++ createAdminUserSP.ps1
@@ -0,0 +1,15 @@
+$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+$currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if (-not $currentUser.IsInRole($adminRole)) {
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition
+    $newProcess.Verb = "runas"
+    [System.Diagnostics.Process]::Start($newProcess)
+    exit
+}
+
+$Secure_String_Pwd = Read-Host -Prompt "Enter password for itadmin user" -AsSecureString
+New-LocalUser -Name 'itadmin' -Description 'Admin account for Sensapure Devices' -Password $Secure_String_Pwd
+Add-LocalGroupMember -Group 'Administrators' -Member 'itadmin'
+
+Remove-Item -Path $MyInvocation.MyCommand.Path -Force
