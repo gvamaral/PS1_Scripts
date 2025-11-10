015bb722c588bc10a015d52a9e08fcb030daa91c
Gabriel Amaral <gamaral@sensapure.com>
2025-11-10 12:35:17 -0700

    adding a Install_rover script for quick install, still need to figure out a way to remotely install Rover correctly through a script without BadUSB injection

---
+++ Install_rover.ps1
@@ -0,0 +1,33 @@
+# Ensure the script is running with administrative privileges
+$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+$currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if (-not $currentUser.IsInRole($adminRole)) {
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition
+    $newProcess.Verb = "runas"
+    [System.Diagnostics.Process]::Start($newProcess)
+    exit
+}
+
+$roverPath = "C:\Apps\rover-installer.msi"
+$roverZipPath = "C:\Apps\rover-installer-2.2.0.zip"
+
+if (Test-Path $roverPath) {
+    Write-Host "Installing Rover from $roverPath"
+    Start-Process "msiexec.exe" -ArgumentList "/i `"$roverPath`"" -Wait
+    Write-Host "Rover 2.2.0 installation completed."
+} else {
+    if (Test-Path $roverZipPath) {
+        Write-Host "Extracting Rover installer from $roverZipPath"
+        Expand-Archive -Path $roverZipPath -DestinationPath "C:\Apps\"
+        if (Test-Path $roverPath) {
+            Write-Host "Installing Rover from extracted MSI at $roverPath"
+            Start-Process "msiexec.exe" -ArgumentList "/i `"$roverPath`"" -Wait
+            Write-Host "Rover 2.2.0 installation completed."
+        } else {
+            Write-Error "Rover installer not found after extraction at $roverPath."
+        }
+    } else {
+    Write-Error "Rover installer not found at $roverPath. Please copy zip from S Drive to C:\Apps."
+    }
+}
