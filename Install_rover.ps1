2a6897473bc8782cdc3fc406ad2f9793504946e3
Gabriel Amaral <gamaral@sensapure.com>
2025-11-11 11:14:04 -0700

    testing it withough the adminRole

--- Install_rover.ps1
+++ Install_rover.ps1
@@ -1,13 +1,13 @@
-# Ensure the script is running with administrative privileges
-$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
-$currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-if (-not $currentUser.IsInRole($adminRole)) {
-    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
-    $newProcess.Arguments = $myInvocation.MyCommand.Definition
-    $newProcess.Verb = "runas"
-    [System.Diagnostics.Process]::Start($newProcess)
-    exit
-}
+# # Ensure the script is running with administrative privileges
+# $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+# $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+# if (-not $currentUser.IsInRole($adminRole)) {
+#     $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
+#     $newProcess.Arguments = $myInvocation.MyCommand.Definition
+#     $newProcess.Verb = "runas"
+#     [System.Diagnostics.Process]::Start($newProcess)
+#     exit
+# }
 
 $roverPath = "C:\Apps\rover-installer.msi"
 $roverZipPath = "C:\Apps\rover-installer-2.2.0.zip"
