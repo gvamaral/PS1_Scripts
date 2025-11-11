63a6ff1f106e7191edb2de02987a0fc334cd680e
Gabriel Amaral <gamaral@sensapure.com>
2025-11-10 17:02:41 -0700

    Added getting rover from S drive if not present on C:\Apps

--- Install_rover.ps1
+++ Install_rover.ps1
@@ -16,7 +16,8 @@ if (Test-Path $roverPath) {
     Write-Host "Installing Rover from $roverPath"
     Start-Process "msiexec.exe" -ArgumentList "/i `"$roverPath`"" -Wait
     Write-Host "Rover 2.2.0 installation completed."
-} else {
+} 
+else {
     if (Test-Path $roverZipPath) {
         Write-Host "Extracting Rover installer from $roverZipPath"
         Expand-Archive -Path $roverZipPath -DestinationPath "C:\Apps\"
@@ -24,10 +25,31 @@ if (Test-Path $roverPath) {
             Write-Host "Installing Rover from extracted MSI at $roverPath"
             Start-Process "msiexec.exe" -ArgumentList "/i `"$roverPath`"" -Wait
             Write-Host "Rover 2.2.0 installation completed."
-        } else {
+        } 
+        else {
             Write-Error "Rover installer not found after extraction at $roverPath."
         }
-    } else {
-    Write-Error "Rover installer not found at $roverPath. Please copy zip from S Drive to C:\Apps."
+    } 
+    else {
+        Write-Host "Rover installer not found at $roverPath. Copying from S: Drive to C:\Apps."
+        $sourcePath = "S:\IT\Software Install\M3\rover-installer-2.2.0.zip"
+        $destinationPath = "C:\Apps\rover-installer-2.2.0.zip"
+
+        if (Test-Path $sourcePath) {
+            Copy-Item -Path $sourcePath -Destination $destinationPath -Force
+            Write-Host "Rover installer copied to $destinationPath. Extracting now."
+            Expand-Archive -Path $destinationPath -DestinationPath "C:\Apps\"
+            if (Test-Path $roverPath) {
+                Write-Host "Installing Rover from extracted MSI at $roverPath"
+                Start-Process "msiexec.exe" -ArgumentList "/i `"$roverPath`"" -Wait
+                Write-Host "Rover 2.2.0 installation completed."
+            } 
+            else {
+                Write-Error "Rover installer not found after extraction at $roverPath."
+            }
+        }
+        else {
+            Write-Error "Source installer not found at $sourcePath."
+        }
     }
 }
